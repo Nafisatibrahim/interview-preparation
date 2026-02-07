800df3a80cbebd6a871570d3613c7b197fa63d5c
Nafisat Ibrahim <nafisat.l@outlook.com>
2026-02-07 14:13:27 -0500

    Add base app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Interview Prep",
+    page_icon=":sparkles:",
+    layout="wide",
+)
+
+st.title("Interview Preparation App")
+st.caption("== Welcome to the Interview Prep App! ==")
